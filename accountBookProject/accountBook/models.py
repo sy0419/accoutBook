8272cdfe8b281aacef69120ca2cd9c4c17efcbbf
sy0419 <psy041912@gmail.com>
2025-08-15 23:05:21 +0900

    Define Transaction model with amount, date, category, and description fields.

--- accountBookProject/accountBook/models.py
+++ accountBookProject/accountBook/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Transaction(models.Model):
+    account = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    
+    CATEGORY_CHOICES = [
+        ('income', 'Income'),
+        ('expense', 'Expense')
+    ]
+
+    category = models.CharField(max_length=7, choices=CATEGORY_CHOICES)
+
+    description = models.CharField(max_length=255, blank=True)
