53e04da29d8400226bb2a2f9b07160e159d4ca25
sy0419 <psy041912@gmail.com>
2025-08-16 17:00:31 +0900

    Add URL pattern for editing transactions with detailed comments.

--- accountBookProject/accountBook/urls.py
+++ accountBookProject/accountBook/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name='home'),  # 홈 페이지 / Home page
     path('transactions/', views.transaction_list, name='transaction_list'),  # 트랜잭션 목록 페이지 / Transaction list page
     path('transactions/add/', views.add_transaction, name='add_transaction'),  # 트랜잭션 추가 페이지 / Transactiono adding page
+    path('transactions/<int:pk>/edit/', views.edit_transaction, name='edit_transaction'),  # 거래 수정 페이지 / Edit transaction page
 ]
