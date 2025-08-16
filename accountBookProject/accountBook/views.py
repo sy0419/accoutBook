1cde985ce9ffe74b0c967789e69dfab94c8b6085
sy0419 <psy041912@gmail.com>
2025-08-16 18:28:28 +0900

    Implement view function for deleting a transaction with confirmation.

--- accountBookProject/accountBook/views.py
+++ accountBookProject/accountBook/views.py
@@ -33,3 +33,11 @@ def edit_transaction(request, pk):
     else:
         form = TransactionForm(instance=transaction) # GET 요청 시 기존 데이터를 채운 폼 생성
         return render(request, 'edit_transaction.html', {'form': form}) # 템플릿에 폼 전달
+
+# 거래 삭제 페이지 / Delete transaction page 
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk) # 삭제할 거래 가져오기 / Get the transaction to delete
+    if request.method == 'POST': # POST 요청일 때 삭제 실행 / Delete on POST request
+        transaction.delete()
+        return redirect('transaction_list') # 삭제 후 거래 목록 페이지로 리다이렉트 / Redirect to transaction list after delete
+    return render(request, 'delete_transaction.html', {'transaction:', transaction}) # 확인 페이지 렌더링 / Render confirmation page
