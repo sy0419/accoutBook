c5ee86a82703176724377fc65c05b82e23b0d464
sy0419 <psy041912@gmail.com>
2025-08-16 16:18:12 +0900

    Add view for handling transaction creation and update URLs.

--- accountBookProject/accountBook/views.py
+++ accountBookProject/accountBook/views.py
@@ -1,12 +1,23 @@
-# render 함수는 템플릿 파일을 렌더링해주는 함수
-from django.shortcuts import render
-from .models import Transaction
+from django.shortcuts import render, redirect
+from .forms import TransactionForm
+from .models import Transaction  
 
-# Create your views here.
+# 홈 페이지 / Home page
 def home(request):
-    # 'home.html' 템플릿을 렌더링해서 반환
     return render(request, 'home.html')
 
+# 트랜잭션 목록 페이지 / Transaction list page
 def transaction_list(request):
-    transactions = Transaction.objects.all()
+    transactions = Transaction.objects.all()  # 트랜잭션 데이터를 모두 가져오기
     return render(request, 'transaction_list.html', {'transactions': transactions})
+
+# 트랜잭션 추가 페이지 / Add transaction page
+def add_transaction(request):
+    if request.method == 'POST':  # POST 요청일 때
+        form = TransactionForm(request.POST)
+        if form.is_valid():  # 폼이 유효하다면
+            form.save()  # 데이터를 저장
+            return redirect('transaction_list')  # 트랜잭션 목록 페이지로 리다이렉트
+    else:
+        form = TransactionForm()  # GET 요청일 때 빈 폼 생성
+    return render(request, 'add_transaction.html', {'form': form})  # 폼을 템플릿에 전달
